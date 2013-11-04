615c56e83319132e5f1c37af0f0095d01c38ec70
Carlos Gregorio Martín Pérez <alu0100702293@ull.edu.es>
2013-11-04 18:43:57 +0000

    Expectativas de instanciacion de clase y operacion suma

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,4 +1,15 @@
 require "matriz"
 
 describe Matriz do
+	it "# instanciacion de una matriz" do
+		@a = Matriz.new(2,2,1,1,2,2)
+	end
+	describe "# operaciones con matrices" do
+		it " suma "do
+			@a = Matriz.new(2,2,1,1,2,2)
+			@b = Matriz.new(2,2,1,1,2,2)
+			@c = Matriz.new(2,2,2,2,4,4)
+			(@a + @b).should == @c
+		end
+	end
 end
